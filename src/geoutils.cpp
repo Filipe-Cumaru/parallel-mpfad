a682564eb5ea18c93910fc9ce45c81871f790d11
Filipe-Cumaru <facsa@cin.ufpe.br>
2019-07-19 17:45:15 -0300

    Using BLAS and overloading normal_vector.
    All operations involving vectors are now performed using BLAS routines.
    The normal_vector function now has three implementations: one that
    accepts the vertices coordinates as a single array, one that accepts a
    reference point and one that accepts a reference point and the vertices
    as three arrays.

--- src/geoutils.cpp
+++ src/geoutils.cpp
@@ -17,32 +17,95 @@ namespace geoutils {
         Calculates the normal vector to a triangular face with vertices's
         coordinates vert_coords and stores the result in n.
     */
-    void get_normal_vector (double vert_coords[9], double n[3]) {
-        double ab[3] = { vert_coords[3] - vert_coords[0],
-            vert_coords[4] - vert_coords[1], vert_coords[5] - vert_coords[2] };
-        double ac[3] = { vert_coords[6] - vert_coords[0],
-            vert_coords[7] - vert_coords[1], vert_coords[8] - vert_coords[2] };
+    void normal_vector (double vert_coords[9], double n[3]) {
+        double ab[3], ac[3];
+
+        cblas_dcopy(3, &vert_coords[0], 1, &ab[0], 1);
+        cblas_dscal(3, -1.0, &ab[0], 1);
+        cblas_daxpy(3, 1, &vert_coords[3], 1, &ab[0], 1);
+
+        cblas_dcopy(3, &vert_coords[0], 1, &ac[0], 1);
+        cblas_dscal(3, -1.0, &ac[0], 1);
+        cblas_daxpy(3, 1, &vert_coords[6], 1, &ac[0], 1);
+
         geoutils::cross_product(ab, ac, n);
+    }
+
+    /*
+        Calculates the normal vector to a triangular face with vertices's
+        coordinates vert_coords corresponding to a reference point and stores
+        the result in n.
+    */
+    void normal_vector (double vert_coords[9], double ref[3], double n[3]) {
+        double ab[3], ac[3], ref_vector[3];
+
+        cblas_dcopy(3, &ref[0], 1, &ref_vector[0], 1);
+        cblas_dscal(3, -1.0, &ref_vector[0], 1);
+        cblas_daxpy(3, 1, &vert_coords[0], 1, &ref_vector[0], 1);
+
+        cblas_dcopy(3, &vert_coords[0], 1, &ab[0], 1);
+        cblas_dscal(3, -1.0, &ab[0], 1);
+        cblas_daxpy(3, 1, &vert_coords[3], 1, &ab[0], 1);
+
+        cblas_dcopy(3, &vert_coords[0], 1, &ac[0], 1);
+        cblas_dscal(3, -1.0, &ac[0], 1);
+        cblas_daxpy(3, 1, &vert_coords[6], 1, &ac[0], 1);
+
+        geoutils::cross_product(ab, ac, n);
+
+        double vector_orientation = cblas_ddot(3, &n[0], 1, &ref_vector[0], 1);
+        if (vector_orientation < 0.0) {
+            cblas_dscal(3, -1.0, &n[0], 1);
+        }
+    }
+
+    void normal_vector (double i[3], double j[3], double k[3], double ref[3], double n[3]) {
+        double ref_vector[3];
+
+        cblas_dcopy(3, &ref[0], 1, &ref_vector[0], 1);
+        cblas_dscal(3, -1.0, &ref_vector[0], 1);
+        cblas_daxpy(3, 1, &i[0], 1, &ref_vector[0], 1);
+
+        cblas_dcopy(3, &i[0], 1, &ab[0], 1);
+        cblas_dscal(3, -1.0, &ab[0], 1);
+        cblas_daxpy(3, 1, &j[0], 1, &ab[0], 1);
+
+        cblas_dcopy(3, &i[0], 1, &ac[0], 1);
+        cblas_dscal(3, -1.0, &ac[0], 1);
+        cblas_daxpy(3, 1, &k[0], 1, &ac[0], 1);
+
+        geoutils::cross_product(ab, ac, n);
+
+        double vector_orientation = cblas_ddot(3, &n[0], 1, &ref_vector[0], 1);
+        if (vector_orientation < 0.0) {
+            cblas_dscal(3, -1.0, &n[0], 1);
+        }
     }
 
     /*
         Returns the area of a face with normal area vector n.
     */
-    double get_face_area (double n[3]) {
+    double face_area (double n[3]) {
         return sqrt(cblas_ddot(3, &n[0], 1, &n[0], 1));
     }
 
     /*
         Calculate the volume of a tetrahedron.
     */
-    double get_tetra_volume (double vert_coords[12]) {
-        double v1[3] = { vert_coords[3] - vert_coords[0],
-            vert_coords[4] - vert_coords[1], vert_coords[5] - vert_coords[2] };
-        double v2[3] = { vert_coords[6] - vert_coords[0],
-            vert_coords[7] - vert_coords[1], vert_coords[8] - vert_coords[2] };
-        double v3[3] = { vert_coords[9] - vert_coords[0],
-            vert_coords[10] - vert_coords[1], vert_coords[11] - vert_coords[2] };
-        double temp[3], volume = 0.0;
+    double tetra_volume (double vert_coords[12]) {
+        double v1[3], v2[3], v3[3], temp[3], volume = 0.0;
+
+        cblas_dcopy(3, &vert_coords[0], 1, &v1[0], 1);
+        cblas_dscal(3, -1.0, &v1[0], 1);
+        cblas_daxpy(3, 1, &vert_coords[3], 1, &v1[0], 1);
+
+        cblas_dcopy(3, &vert_coords[0], 1, &v2[0], 1);
+        cblas_dscal(3, -1.0, &v2[0], 1);
+        cblas_daxpy(3, 1, &vert_coords[6], 1, &v2[0], 1);
+
+        cblas_dcopy(3, &vert_coords[0], 1, &v3[0], 1);
+        cblas_dscal(3, -1.0, &v3[0], 1);
+        cblas_daxpy(3, 1, &vert_coords[9], 1, &v3[0], 1);
 
         geoutils::cross_product(v1, v2, temp);
         volume = fabs(cblas_ddot(3, &temp[0], 1, &v3[0], 1)) / 6.0;
--- src/geoutils.h
+++ src/geoutils.h
@@ -13,9 +13,11 @@ using namespace std;
 
 namespace geoutils {
     void cross_product(double u[3], double v[3], double n[3]);
-    void get_normal_vector (double vert_coords[9], double n[3]);
-    double get_face_area (double n[3]);
-    double get_tetra_volume (double vert_coords[12]);
+    void normal_vector (double vert_coords[9], double n[3]);
+    void normal_vector (double vert_coords[9], double ref[3], double n[3]);
+    void normal_vector (double i[3], double j[3], double k[3], double ref[3], double n[3]);
+    double face_area (double n[3]);
+    double tetra_volume (double vert_coords[12]);
 }
 
 #endif
